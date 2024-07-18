1b40faaa1d92cf699582ace7701ac49b86ae3b7a
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-18 00:08:22 -0700

    Finished styling for resume page

--- Lovely-React-Portfolio/src/components/Resume.jsx
+++ Lovely-React-Portfolio/src/components/Resume.jsx
@@ -1,8 +1,18 @@
 import REACT from "react";
-
+import "./Resume.css"
 const Resume = () => {
     return (
-        <h2>Resume</h2>
+        <div>
+            <h2>Lovely's Resume</h2>
+            <iframe
+            title="Resume PDF"
+            width="400px"
+            height="500px"
+            src="/assets/images/LovelyResume.pdf"
+            >
+                This browser does not support PDFs. Please download.
+            </iframe>
+        </div>
     )
 }
 
