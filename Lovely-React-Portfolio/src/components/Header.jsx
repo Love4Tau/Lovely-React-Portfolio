73b4ad9e5a727a7f8dca9c749b48ccefb89c1e64
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-17 20:50:53 -0700

    Created header and set up app

---
+++ Lovely-React-Portfolio/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import REACT from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>LOVELY TAUTUA'A</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a>About me</a>
+                    </li>
+
+                    <li>
+                        <a>Portfolio</a>
+                    </li>
+
+                    <li>
+                        <a>Contact</a>
+                    </li>
+
+                    <li>
+                        <a>Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
